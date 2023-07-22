e01fec61d0bcd1c603e0b71f64e70ebc22cd5165
MishraTanishq619 <mishratanishq619@gmail.com>
2023-07-22 09:51:41 -0400

    finally

--- final_project/server.py
+++ final_project/server.py
@@ -2,7 +2,7 @@ from machinetranslation.translator import english_to_french, french_to_english
 from flask import Flask, render_template, request
 import json
 
-app = Flask("Web Translator")
+app = Flask("Web Translator",static_folder="static", template_folder="templates")
 
 @app.route("/englishToFrench")
 def englishToFrench():
@@ -21,6 +21,7 @@ def frenchToEnglish():
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
